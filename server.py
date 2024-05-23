3c4d174fab698c451d96d4c46486e8babf78d950
Maurice Götze <99mg24@web.de>
2024-05-23 17:27:36 +0200

    dev: hochladen von bildern in gallery möglich

--- server.py
+++ server.py
@@ -59,21 +59,55 @@ def upload_file():
             return render_template('index.html', date=selectedDate, event=event_str)
 
     return
+@app.route('/gallery', methods=['POST'])
+def upload_file_toGallery():
+    global selectedDate
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file: #and allowed_file(file.filename):
+            #filename = secure_filename(file.filename)
+            filename = file.filename
+            event_path = os.path.join(app.config['ROOT_DIR'],selectedDate)
+
+            if not os.path.exists(event_path):
+                os.mkdir(event_path) 
+
+            file.save(os.path.join(event_path,filename))
+            image_paths=getPathsFromDir(event_path)
+        return render_template('gallery.html', paths=image_paths, date=selectedDate)
+
+    return
+
+def getPathsFromDir(dir):
+    image_paths = []
+    for root,dirs,files in os.walk(dir):
+        for file in files:
+            if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
+                image_paths.append(encode(os.path.join(root,file)))
+
+    return image_paths
+
 
 @app.route('/gallery')
 def home():
     global selectedDate
     root_dir = app.config['ROOT_DIR']
-    image_paths = []
+
     if selectedDate == "":
         return redirect("/")
     else:
         event_path = os.path.join(root_dir,selectedDate)
+        image_paths=getPathsFromDir(event_path)
 
-        for root,dirs,files in os.walk(event_path):
-            for file in files:
-                if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
-                    image_paths.append(encode(os.path.join(root,file)))
         return render_template('gallery.html', paths=image_paths, date=selectedDate)
 
 
