0522f08616032e7f4b52b076bd46e7a279904298
Maurice Götze <99mg24@web.de>
2024-05-23 16:52:18 +0200

    dev: anzeige von bildern eines dir

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template,request,redirect,flash,url_for
+from flask import Flask, render_template,request,redirect,flash,url_for,send_from_directory
 from flask_socketio import SocketIO, emit
 #!/usr/bin/python
 import sqlite3
 import os,sys
 import requests,time
 import json, datetime
-
+import binascii
 import main_test
 
 UPLOAD_FOLDER = 'uploads'
+ROOT_DIR = 'events'
 app = Flask(__name__)
 #            static_url_path='',
 #            static_folder='/static')
 app.config['SECRET_KEY'] = 'secret!'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ROOT_DIR'] = ROOT_DIR
+app.config['IMAGE_EXTS'] = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
 socketio = SocketIO(app)
+
+def encode(x):
+    return binascii.hexlify(x.encode('utf-8')).decode()
+
+def decode(x):
+    return binascii.unhexlify(x.encode('utf-8')).decode()
+
 
 @app.route('/')
 def index():
@@ -38,14 +48,26 @@ def upload_file():
             filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             date,event_str = main_test.main()
-
             print(date,event_str)
-            socketio.emit('event_update', {'date_str': date.strftime('%Y-%m-%d'),'event_str': str(event_str)})
-
-            #return redirect(url_for('download_file', name=filename))
-            return redirect("/"+ date.strftime('%Y-%m-%d'))
+            return render_template('index.html', date=date.strftime('%Y-%m-%d'), event=event_str)
 
     return
+
+@app.route('/gallery')
+def home():
+    root_dir = app.config['ROOT_DIR']
+    image_paths = []
+    for root,dirs,files in os.walk(root_dir):
+        for file in files:
+            if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
+                image_paths.append(encode(os.path.join(root,file)))
+    return render_template('gallery.html', paths=image_paths)
+
+
+@app.route('/cdn/<path:filepath>')
+def download_file(filepath):
+    dir,filename = os.path.split(decode(filepath))
+    return send_from_directory(dir, filename, as_attachment=False)
 
 if __name__ == '__main__':
     socketio.run(app,host="0.0.0.0",allow_unsafe_werkzeug=True)
--- static/script.js
+++ static/script.js
@@ -3,10 +3,42 @@ console.log("INIT");
 console.log(document.getElementById("date1234"));
 
 socket.on('event_update', function(msg) {
-    console.log(msg);
-    document.getElementById('date1234').value = msg.date_str;
-    document.getElementById('event').value = msg.event_str;
+    //console.log(msg);
+    //document.getElementById('date1234').value = msg.date_str;
+    //document.getElementById('event').value = msg.event_str;
 
 
 
   });
+
+
+  function clicked_img(img,fp){
+    console.log(img.src);
+
+    var top=document.getElementById('top')
+
+    top.src = img.src;
+
+    top.hidden=false;
+
+
+    if (img.naturalWidth<screen.width*0.6 && img.naturalHeight<screen.height*0.6) {
+
+      top.width=img.naturalWidth;
+      top.height=img.naturalHeight;
+
+    } else {
+
+      top.width=screen.width*0.6;
+      top.height=img.naturalHeight/img.naturalWidth*top.width;
+
+    }
+
+    document.getElementById('close').hidden = false;
+}
+
+
+function do_close(){
+document.getElementById('top').hidden=true;
+document.getElementById('close').hidden=true;
+}
